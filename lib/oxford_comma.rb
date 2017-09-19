eeec9a104174a11ac169cc368bd93bb702e31d66
alpbourne <alpbourne@gmail.com>
2017-09-19 23:15:53 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
   if array.length == 1
     array.join
-  if array.length == 2
+  elsif array.length == 2
     array.join(" and ")
-  elsif array.length > 3
+  else array.length > 3
     a = array.pop
     array.join(", ") + ", and " + f
   end
